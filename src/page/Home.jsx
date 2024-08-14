61bb4fc58699e592fcbb499c05657bb11bcb2b0c
Mizero Lucky <mizeroluckygall003@gmail.com>
2024-08-14 16:06:19 +0200

    adding name:

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,6 +5,7 @@ import Smallcontact from '../components/Smallcontact';
 const Home = () => {
   return (
     <div className='ma'>
+      <h1>mlucky</h1>
       <Welcome />
       <Smallcontact />
     </div>
